6b53c1dd8a9d2fab0e0529d2c6267919175b3286
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-11-01 10:25:20 -0500

    Created typeDefs Schema and resolvers based on user-routes and Models

--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
+const db = require("./config/connection");
+
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(cors());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+  introspection: true,
 });
+
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  // Apply Apollo Server middleware to the "/graphql" path
+  server.applyMiddleware({ app });
+
+  // Parse JSON and URL-encoded data
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // Serve static assets in production
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+    
+    // Route all requests to the React app's HTML file for client-side routing
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
+
+  // Set up your database connection
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
